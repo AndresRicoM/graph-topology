051040e2112dbe8042cb5c98be96b85b1088296d
AndresRicoM <andres.rico.m@gmail.com>
2020-04-13 20:25:20 -0400

    added boston data

--- demo.py
+++ demo.py
@@ -2,11 +2,9 @@ import numpy as np
 import math
 import random
 import matplotlib.pyplot as plt
-import networkx as nx
 import numpy as np
 import random
 from population_simulation import *
-from mayavi import mlab
 from search import *
 import os
 
