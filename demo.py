9b8fc2993e703b226bbecc1e85dc50f037795d41
AndresRicoM <andres.rico.m@gmail.com>
2020-04-13 19:18:23 -0500

    fix large bingo

--- demo.py
+++ demo.py
@@ -19,11 +19,11 @@ if __name__ == '__main__':
 
     main_path = os.getcwd()
     list_path = main_path + '/populations/lists/' + '/' + pop_name + '.txt'
-    mat_path = main_path + '/populations/matrices/' + '/' + pop_name + '.txt'
+    #mat_path = main_path + '/populations/matrices/' + '/' + pop_name + '.txt'
     inf_path = main_path + '/populations/infected/' + '/' + pop_name + '.txt'
 
     list = get_poplist(list_path)
-    mat = get_popmat(mat_path)
+    #mat = get_popmat(mat_path)
     infect = get_infectvect(inf_path)
 
     print('Beacon: ', beacon, 'Quarentine: ' , bingo(beacon,list,infect))
